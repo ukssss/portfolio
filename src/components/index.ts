4929e4d66b57c30e3f7ab0b7d4a3af2020f901de
ukssss <1872003115su@gmail.com>
2023-09-13 16:23:21 +0900

    Feat | Error Components 제작

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Error } from './common/Error';
-export { default as Footer } from './common/Footer';
 export { default as Hamburger } from './common/Hamburger';
 export { default as Header } from './common/Header';
 export { default as SideCar } from './common/SideCar';
