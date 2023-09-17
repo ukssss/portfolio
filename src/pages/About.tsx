22231fdcd9748f65dd6609907bdee6082e57c27c
ukssss <1872003115su@gmail.com>
2023-09-18 00:43:39 +0900

    Docs | About 내용 작성

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,58 +5,190 @@ const About = () => {
     return (
         <Container>
             <Section>
-                <SubTitle>Awards</SubTitle>
+                <SubTitle>Experience</SubTitle>
                 <List>
-                    <Item>
-                        <ItemLeft>2022-11</ItemLeft>
-                        <ItemRight>2022 PKNU CE S/W 경진대회 우수상</ItemRight>
-                    </Item>
-                    <Item>
-                        <ItemLeft>2021-10</ItemLeft>
-                        <ItemRight>2021 PNU AI · 오픈소스 SW 해커톤 장려상</ItemRight>
-                    </Item>
+                    <AboutMe>
+                        <AboutMeDate>2017.03 ~ 2023.02</AboutMeDate>
+                        <AboutMeInfo>
+                            <AboutMeMain>부경대학교 대연캠퍼스</AboutMeMain>
+                            <AboutMeSub>컴퓨터공학전공 컴퓨터공학과 학사 졸업</AboutMeSub>
+                        </AboutMeInfo>
+                    </AboutMe>
+
+                    <AboutMe>
+                        <AboutMeDate>2021.09 ~ 2021.12</AboutMeDate>
+                        <AboutMeInfo>
+                            <AboutMeMain>GDSC PKNU</AboutMeMain>
+                            <AboutMeSub>
+                                <AboutMeSubItem>React 기초에 대해 학습했습니다.</AboutMeSubItem>
+                                <AboutMeSubItem>
+                                    간단한 웹 게임을 만들며 React를 학습해볼 수 있었습니다.
+                                </AboutMeSubItem>
+                            </AboutMeSub>
+                        </AboutMeInfo>
+                    </AboutMe>
+
+                    <AboutMe>
+                        <AboutMeDate>2022.09 ~ 2022.12</AboutMeDate>
+                        <AboutMeInfo>
+                            <AboutMeMain>노마드 스터디 2기</AboutMeMain>
+                            <AboutMeSub>
+                                <AboutMeSubItem>
+                                    웹 사이트의 내용을 브라우저에 전달하는 원리에 대해
+                                    학습하였습니다.
+                                </AboutMeSubItem>
+                                <AboutMeSubItem>
+                                    CSS Layout 을 심도 있게 학습하여 다양한 CSS 기법을
+                                    학습하였습니다.
+                                </AboutMeSubItem>
+                                <AboutMeSubItem>
+                                    Node.js를 학습하여 JS를 통해 애플리케이션의 프론트엔드, 백엔드
+                                    모두에서 사용할 수 있는 스킬을 습득하였습니다.
+                                </AboutMeSubItem>
+                            </AboutMeSub>
+                        </AboutMeInfo>
+                    </AboutMe>
+
+                    <AboutMe>
+                        <AboutMeDate>2022.11 ~ 2023.03</AboutMeDate>
+                        <AboutMeInfo>
+                            <AboutMeMain>멋쟁이사자처럼 프론트엔드 스쿨 4기</AboutMeMain>
+                            <AboutMeSub>
+                                <AboutMeSubItem>
+                                    웹 접근성에 대해 학습하고 이를 고려한 HTML 마크업, CSS
+                                    스타일링에 대해 학습했습니다.
+                                </AboutMeSubItem>
+
+                                <AboutMeSubItem>
+                                    flex, grid 등 다양한 CSS 속성을 배우고 이를 활용한 스타일링
+                                    기법을 학습했습니다.
+                                </AboutMeSubItem>
+
+                                <AboutMeSubItem>
+                                    자바스크립트 코어와 함수형 프로그래밍을 학습하여 자바스크립트를
+                                    더욱 깊게 이해할 수 있었습니다.
+                                </AboutMeSubItem>
+
+                                <AboutMeSubItem>
+                                    React에서 DOM을 어떻게 렌더링하고 브라우저 이벤트를 처리하는
+                                    과정을 학습하였다.
+                                </AboutMeSubItem>
+
+                                <AboutMeSubItem>
+                                    CRA, Vite 기법을 배우고, 장단점을 파악하며 이를 활용하여
+                                    프로젝트를 진행하였다.
+                                </AboutMeSubItem>
+
+                                <AboutMeSubItem>
+                                    팀을 구성해 팀프로젝트를 진행하여 협업 경험을 쌓고 팀원들과
+                                    다양한 전문성을 공유하고 지식을 쌓을 수 있었던 계기였다.
+                                </AboutMeSubItem>
+                            </AboutMeSub>
+                        </AboutMeInfo>
+                    </AboutMe>
+
+                    <AboutMe>
+                        <AboutMeDate>2023.08 ~ 2023.09</AboutMeDate>
+                        <AboutMeInfo>
+                            <AboutMeMain>원티드 프리온보딩 인턴쉽 프론트엔드 12차</AboutMeMain>
+                            <AboutMeSub>
+                                <AboutMeSubItem>
+                                    협업 능력, 효율적인 소통을 위해서 어떤 부분들을 시도해 볼 수
+                                    있는지 생각해보고 그와 관련된 다양한 기법을 학습하였다.
+                                </AboutMeSubItem>
+
+                                <AboutMeSubItem>
+                                    리액트 렌더링에 대해 학습하고 렌더링 최적화 기법을 배우고 다양한
+                                    Hook 기법들을 학습하였다.
+                                </AboutMeSubItem>
+
+                                <AboutMeSubItem>
+                                    관심사의 분리라는 단어에 대해 고찰해보고, 좋은 소프트웨어를
+                                    개발하기 위해 어떠한 방식으로 개발을 진행해야 할 지 생각해 볼 수
+                                    있었던 시간이었다.
+                                </AboutMeSubItem>
+
+                                <AboutMeSubItem>
+                                    타입스크립트의 동작방식을 배우고 프로젝트를 진행해보며 함수에서
+                                    타입을 어떻게 지정하고 코드 안정성을 갖춘 코드를 갖추기 위해
+                                    학습하였다.
+                                </AboutMeSubItem>
+                            </AboutMeSub>
+                        </AboutMeInfo>
+                    </AboutMe>
                 </List>
             </Section>
             <Section>
                 <SubTitle>Certificates</SubTitle>
                 <List>
-                    <Item>
-                        <ItemLeft>2023-09</ItemLeft>
-                        <ItemRight>정보처리기사</ItemRight>
-                    </Item>
-                    <Item>
-                        <ItemLeft>2022-09</ItemLeft>
-                        <ItemRight>SQL 개발자 (SQLD)</ItemRight>
-                    </Item>
-                    <Item>
-                        <ItemLeft>2022-02</ItemLeft>
-                        <ItemRight>컴퓨터활용능력 1급</ItemRight>
-                    </Item>
+                    <AboutMe>
+                        <AboutMeDate>2022.02</AboutMeDate>
+                        <AboutMeInfo>
+                            <AboutMeMain>컴퓨터활용능력 1급</AboutMeMain>
+                            <AboutMeSub>대한상공회의소</AboutMeSub>
+                        </AboutMeInfo>
+                    </AboutMe>
+
+                    <AboutMe>
+                        <AboutMeDate>2022.06</AboutMeDate>
+                        <AboutMeInfo>
+                            <AboutMeMain>한국사능력검정 2급</AboutMeMain>
+                            <AboutMeSub>국사편찬위원회</AboutMeSub>
+                        </AboutMeInfo>
+                    </AboutMe>
+
+                    <AboutMe>
+                        <AboutMeDate>2022.09</AboutMeDate>
+                        <AboutMeInfo>
+                            <AboutMeMain>SQL 개발자 (SQLD)</AboutMeMain>
+                            <AboutMeSub>한국데이터산업진흥원</AboutMeSub>
+                        </AboutMeInfo>
+                    </AboutMe>
+
+                    <AboutMe>
+                        <AboutMeDate>2023.09</AboutMeDate>
+                        <AboutMeInfo>
+                            <AboutMeMain>정보처리기사</AboutMeMain>
+                            <AboutMeSub>한국산업인력공단</AboutMeSub>
+                        </AboutMeInfo>
+                    </AboutMe>
                 </List>
             </Section>
             <Section>
-                <SubTitle>Experience</SubTitle>
+                <SubTitle>Awards</SubTitle>
                 <List>
-                    <Item>
-                        <ItemLeft>2023-08 ~ 2023-09</ItemLeft>
-                        <ItemRight>
-                            원티드 프리온보딩 인턴쉽 프론트엔드 (React, Typescript)
-                        </ItemRight>
-                    </Item>
-                    <Item>
-                        <ItemLeft>2022-11 ~ 2023-03</ItemLeft>
-                        <ItemRight>
-                            멋쟁이사자처럼 프론트엔드 스쿨 (HTML, CSS, Javascript, React)
-                        </ItemRight>
-                    </Item>
-                    <Item>
-                        <ItemLeft>2022-09 ~ 2022-12</ItemLeft>
-                        <ItemRight>노마드 스터디 (HTML, CSS, Javascript)</ItemRight>
-                    </Item>
-                    <Item>
-                        <ItemLeft>2021-09 ~ 2021-12</ItemLeft>
-                        <ItemRight>GDSC PKNU (React)</ItemRight>
-                    </Item>
+                    <AboutMe>
+                        <AboutMeDate>2021.10</AboutMeDate>
+                        <AboutMeInfo>
+                            <AboutMeMain>2021 PNU AI · 오픈소스 SW 해커톤 장려상</AboutMeMain>
+                            <AboutMeSub>
+                                <AboutMeSubItem>
+                                    부산광역시 공영주차장 정보 시스템을 제작
+                                </AboutMeSubItem>
+                                <AboutMeSubItem>
+                                    유저들에게 실시간 주차가능여부, 주차장 네비게이션, 주차 관련
+                                    편의 기능 등의 기능을 제공하는 어플리케이션을 제작함. (Android
+                                    Java)
+                                </AboutMeSubItem>
+                            </AboutMeSub>
+                        </AboutMeInfo>
+                    </AboutMe>
+
+                    <AboutMe>
+                        <AboutMeDate>2022.11</AboutMeDate>
+                        <AboutMeInfo>
+                            <AboutMeMain>2022 PKNU CE S/W 경진대회 우수상</AboutMeMain>
+                            <AboutMeSub>
+                                <AboutMeSubItem>
+                                    킥보드 단속 어플리케이션 (제목: 킥라니)를 제작
+                                </AboutMeSubItem>
+                                <AboutMeSubItem>
+                                    WAS(Web Application Server) 파트를 담당하였습니다. (Node.js
+                                    사용)
+                                </AboutMeSubItem>
+                            </AboutMeSub>
+                        </AboutMeInfo>
+                    </AboutMe>
                 </List>
             </Section>
 
@@ -76,23 +208,25 @@ const Section = styled.section``;
 const List = styled.ul`
     margin-bottom: 1.58rem;
 `;
-const Item = styled.li`
+const AboutMe = styled.li`
     padding: 1rem;
     display: flex;
     flex-direction: row;
-    gap: 10px;
+    gap: 0.5rem;
 `;
-const ItemLeft = styled.div`
+const AboutMeDate = styled.div`
+    min-width: 6rem;
     padding: 0 1rem;
     border-right: 1px solid #666;
 
     color: #666;
     font-size: 0.8rem;
 `;
-const ItemRight = styled.div`
-    padding: 1rem;
-    flex: 1;
-    font-weight: 600;
+const AboutMeInfo = styled.div``;
+const AboutMeMain = styled.h4``;
+const AboutMeSub = styled.ul`
+    font-size: 0.8rem;
 `;
+const AboutMeSubItem = styled.li``;
 
 export default About;
