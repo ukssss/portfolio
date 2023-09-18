7e3e93ed9def323f06d6c1575f5f5ff11fbb58e5
ukss <1872003115su@gmail.com>
2023-09-18 16:53:55 +0900

    Revert "First Release (v1.0.0)"

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,5 @@
-import router from '@/routes/router';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
-import './fonts/SUITE.css';
+import App from './App.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <RecoilRoot>
-        <RouterProvider router={router} />
-    </RecoilRoot>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
