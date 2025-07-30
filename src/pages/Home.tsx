c7323a12caf168ad6ea5cc5027e8a0b56df42102
ukssss <1872003115su@gmail.com>
2025-07-30 21:35:02 +0900

    Fix | Vercel 버전 에러 22.X 로 업그레이드 진행

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,6 @@ import { useMediaQuery } from 'react-responsive';
 import styled from 'styled-components'; 
 
 const Home = () => {
-    const isMobile = useMediaQuery({
-        query: '(max-width: 767px)',
-    });
-
-    const isTablet = useMediaQuery({
-        query: '(min-width: 768px) and (max-width: 1023px)',
-    });
-
-    const isDesktop = useMediaQuery({
-        query: '(min-width: 1024px)',
-    });
-
     const isLargeDesktop = useMediaQuery({
         query: '(min-width: 1201px)',
     });
